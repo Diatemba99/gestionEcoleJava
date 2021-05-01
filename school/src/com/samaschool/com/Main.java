068345ddc854cacbf0156562497f1093dde52fef
Diatemba99 <martin.diatta99@gmail.com>
2021-05-01 09:52:09 +0000

    changing menu

--- school/src/com/samaschool/com/Main.java
+++ school/src/com/samaschool/com/Main.java
@@ -15,16 +15,16 @@ public class Main {
     //Fonction d'affichage du menu
     public static  void menuPrincipal()
     {
-        System.out.println("************************************");
-        System.out.println("************************************");
-        System.out.println("1- Gestion Groupe ");
-        System.out.println("2- Gestion Etudiant");
-        System.out.println("3- Gestion Professeurs");
-        System.out.println("4- Gestion Groupe-Etudiant");
-        System.out.println("5- Quitter");
-        System.out.println("Faites votre choix: 5");
-        System.out.println("************************************");
-        System.out.println("************************************");
+        System.out.println("******************************************************************");
+        System.out.println("******************************************************************");
+        System.out.println("---- * ------- * 1- Gestion Groupe             ---- * ------- * --");
+        System.out.println("---- * ------- * 2- Gestion Etudiant           ---- * ------- * --");
+        System.out.println("---- * ------- * 3- Gestion Professeurs        ---- * ------- * --");
+        System.out.println("---- * ------- * 4- Gestion Groupe-Etudiant    ---- * ------- * --");
+        System.out.println("---- * ------- * 5- Quitter                    ---- * ------- * --");
+        System.out.println("---- * ------- * Faites votre choix: 5         ---- * ------- * --");
+        System.out.println("******************************************************************");
+        System.out.println("******************************************************************");
     }
 
 
