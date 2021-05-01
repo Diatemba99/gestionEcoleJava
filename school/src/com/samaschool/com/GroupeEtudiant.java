28cf65f797ef53f7c94cf9e3933478a42d966798
Diatemba99 <martin.diatta99@gmail.com>
2021-05-01 09:58:45 +0000

    adding constructor, getter and Setter for GroupeEtudiant

--- school/src/com/samaschool/com/GroupeEtudiant.java
+++ school/src/com/samaschool/com/GroupeEtudiant.java
@@ -1,4 +1,28 @@
 package com.samaschool.com;
 
 public class GroupeEtudiant {
+    private int ID;
+    private String nomGroupe;
+
+    public GroupeEtudiant(int ID, String nomGroupe) {
+
+        this.ID = ID;
+        this.nomGroupe = nomGroupe;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNomGroupe() {
+        return nomGroupe;
+    }
+
+    public void setNomGroupe(String nomGroupe) {
+        this.nomGroupe = nomGroupe;
+    }
 }
